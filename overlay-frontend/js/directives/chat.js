68c9e37f18767cfc731abf402aae24363a516605
Julian Hollmann <hollmann@uxebu.com>
2015-02-13 14:18:23 +0100

    only show chat lines not older than 10 minutes and hide chat frame completely when no chatlines are visible

--- overlay-frontend/js/directives/chat.js
+++ overlay-frontend/js/directives/chat.js
@@ -6,9 +6,16 @@ TwitchOverlay.directive('chat', [function() {
         },
         templateUrl: 'templates/directives/chat.html',
         link: function($scope, elem, attrs) {
-            $scope.$watch('chatData', function() {
-               console.log($scope);
-            });
+            $scope.showChatFrame = function() {
+                var visibleLines = $scope.chatData.filter(function(line) {
+                    return $scope.showLine(line.ts);
+                });
+                return visibleLines.length > 0;
+            };
+            $scope.showLine = function(ts) {
+                // only show lines if they are not older than 10 minutes
+                return ts > +new Date().getTime() - 1000 * 60 * 10;
+            }
         }
     }
 }]);
