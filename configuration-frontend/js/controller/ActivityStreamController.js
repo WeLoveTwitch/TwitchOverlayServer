cd913049a80be863bfc01f40922f674e5a6ce9b7
Julian Hollmann <hollmann@uxebu.com>
2015-02-15 14:07:21 +0100

    fixes #6

--- configuration-frontend/js/controller/ActivityStreamController.js
+++ configuration-frontend/js/controller/ActivityStreamController.js
@@ -1,14 +1,41 @@
-TwitchOverlay.controller('ActivityStreamController', ['$scope', 'TwitchOverlayServer', 'Tick', function($scope, TwitchOverlayServer, Tick) {
+TwitchOverlay.controller('ActivityStreamController', ['$scope', 'TwitchOverlayServer', 'Tick', function ($scope, TwitchOverlayServer, Tick) {
 
     $scope.followers = [];
 
+    // @TODO: refactor this into a service
+    var knownFollowers = [];
+    var newFollowers = [];
+
+    var initialized = false;
+
     function loadFollowers() {
-        TwitchOverlayServer.getFollowers(function(follower) {
-            $scope.followers = follower;
+        TwitchOverlayServer.getFollowers(function (followers) {
+            checkForNew(followers);
+            $scope.followers = followers;
         });
     }
 
-    Tick.register(function() {
+    function checkForNew(followers) {
+        if(!initialized) {
+            initialized = true;
+            followers.forEach(function (follower) {
+                knownFollowers.push(follower.name);
+            });
+            return;
+        }
+        followers.forEach(function (follower) {
+            if(knownFollowers.indexOf(follower.name) !== -1) {
+                return;
+            }
+            newFollowers.push(follower.name);
+        });
+    }
+
+    $scope.checkNew = function(name) {
+        return newFollowers.indexOf(name) > -1;
+    };
+
+    Tick.register(function () {
         loadFollowers();
     });
 
