35d700fd991c3e763cdc260b10f55a415b8c9cb6
Devon Zara <hello@devonzara.com>
2015-02-16 00:37:53 -0500

    Remove title button focus state on click

--- configuration-frontend/js/directives/header.js
+++ configuration-frontend/js/directives/header.js
@@ -2,9 +2,13 @@ TwitchOverlay.directive('header', ['TwitchOverlayServer', function(TwitchOverlay
     return {
         restrict: 'E',
         templateUrl: 'configuration-frontend/templates/directives/header.html',
-        link: function($scope) {
+        link: function($scope, element) {
             var gui = require('nw.gui');
             var win = gui.Window.get();
+
+            element.on("click", function(event) {
+                element.find("button:focus").blur();
+            });
 
             $scope.goFullscreen = function() {
                 win.toggleFullscreen();
