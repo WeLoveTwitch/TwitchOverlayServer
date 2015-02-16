8c432d9706e3dae1e8dd85c5de21c8be130ba203
Julian Hollmann <hollmann@uxebu.com>
2015-02-16 20:49:19 +0100

    removed "open overlay" button as overlay isn't part of the server anymore

--- frontend/js/controller/HomeController.js
+++ frontend/js/controller/HomeController.js
@@ -1,32 +1,6 @@
 TwitchOverlay.controller('HomeController', ['$scope', 'TwitchOverlayServer', function($scope, TwitchOverlayServer) {
 
     var gui = require('nw.gui');
-    $scope.win = null;
     $scope.server = TwitchOverlayServer;
-
-    $scope.openOverlay = function() {
-        $scope.win = gui.Window.open('./frontend/index.html', {
-            position: 'center',
-            width: 1280,
-            height: 720,
-            "toolbar": false,
-            "frame": false
-        });
-
-        $scope.win.on('closed', function() {
-            $scope.win = null;
-
-            if (!$scope.$$phase) {
-                $scope.$apply();
-            }
-        });
-    };
-
-    $scope.closeOverlay = function() {
-        if(!$scope.win) return false;
-
-        $scope.win.close();
-        $scope.win = null;
-    }
 
 }]);
